d5d2a49d36af0b022f699d9a35d1d213f2b64894
Samir Hesimov <samirhesimov257@gmail.com>
2026-05-21 15:36:55 +0400

    1 day

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String word="slam";
+        int x=word.length()/2;
+        if (word.length()%2!=0){
+            System.out.println(word.charAt(x)+"");
+        }else System.out.println(word.charAt(x-1)+""+word.charAt(x));
+
+
+
+
+//        /*
+//        verilmis arrayda ancaq 1 dene cut qalani tek (ve ya tersine) ededler olacaq aralarindaki 1 deneni tap
+//         */
+//        int [] arr={2,4,6,8,0,12,14,7};
+//        var oddList=new ArrayList<Integer>();
+//        var evenList=new ArrayList<Integer>();
+//
+//        for (int i=0;i<arr.length;i++){
+//           if (arr[i]%2==0) evenList.add(arr[i]);
+//           else oddList.add(arr[i]);
+//
+//        }
+//        if (oddList.size()<2) System.out.println(oddList.getFirst());
+//        else System.out.println(evenList.getFirst());
+
+
+
+//
+//        /*
+//        verilmmis stringin icinde sait olub olmadigini yoxlanmasi
+//         */
+//        String word = "sa;oufihaenufnaskldnfm;iawnfsajkndfm;o";
+//        int count =0;
+//        for(int i=0; i<word.length();i++){
+//            char ch=word.charAt(i);
+//            if (ch=='a' || ch=='i' || ch=='e' || ch=='o' || ch=='u' ){
+//                count++;
+//            }
+//        }
+//        System.out.println(word.indexOf("o"));
+//        System.out.println(word.indexOf("o"));
+
+    }
+}
